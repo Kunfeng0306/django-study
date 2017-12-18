806b82176e1c56aa8eca6958f5f0760ef8ad8d42
lilyef2000 <lilyef2000@gmail.com>
2017-12-18 18:07:33 +0800

    course list-lessons

--- mysite/course/models.py
+++ mysite/course/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from .fields import OrderField
 
 from slugify import slugify
 
@@ -22,3 +23,22 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+def user_directory_path(instance, filename):
+    return "courses/user_{0}/{1}".format(instance.user.id, filename)
+
+class Lesson(models.Model):
+    user = models.ForeignKey(User, related_name='lesson_user')
+    course = models.ForeignKey(Course, related_name='lesson')
+    title = models.CharField(max_length=200)
+    video = models.FileField(upload_to=user_directory_path)
+    description = models.TextField(blank=True)
+    attach = models.FileField(blank=True, upload_to=user_directory_path)
+    created = models.DateTimeField(auto_now_add=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']	
+
+    def __str__(self):
+        return '{}.{}'.format(self.order, self.title)
--- mysite/course/views.py
+++ mysite/course/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views.generic import TemplateView, ListView
-from .models import Course
-from .forms import CreateCourseForm
+from .models import Course, Lesson
+from .forms import CreateCourseForm, CreateLessonForm
+from django.views import View 
 from django.views.generic.edit import CreateView,DeleteView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.base import TemplateResponseMixin
+from django.http import HttpResponse
 
 from braces.views import LoginRequiredMixin
+import json
 # Create your views here.
 class AboutView(TemplateView):
     template_name = "course/about.html"
@@ -41,7 +46,39 @@ class CreateCourseView(UserCourseMixin, CreateView):
         return self.render_to_response({"form":form})
         
 class DeleteCourseView(UserCourseMixin, DeleteView):   
-    template_name = 'course/manage/delete_course_confirm.html'
+    #template_name = 'course/manage/delete_course_confirm.html'
     success_url = reverse_lazy("course:manage_course")
+    
+    def dispatch(self, *args, **kwargs):   
+        resp = super(DeleteCourseView, self).dispatch(*args, **kwargs)   
+        if self.request.is_ajax():   
+            response_data = {"result": "ok"}
+            return HttpResponse(json.dumps(response_data), content_type="application/json")
+        else:
+            return resp
+            
+class CreateLessonView(LoginRequiredMixin, View):    
+    model = Lesson
+    login_url = "/account/login/"
+
+    def get(self, request , *args, **kwargs):   
+        form = CreateLessonForm(user=self.request.user)    
+        return render(request, "course/manage/create_lesson.html", {"form":form})
+
+    def post(self, request, *args, **kwargs):    
+        form = CreateLessonForm(self.request.user, request.POST, request.FILES)    
+        if form.is_valid():
+            new_lesson = form.save(commit=False)
+            new_lesson.user = self.request.user
+            new_lesson.save()
+            return redirect("course:manage_course")
+            
+class ListLessonsView(LoginRequiredMixin, TemplateResponseMixin, View):
+    login_url = "/account/login/"
+    template_name = 'course/manage/list_lessons.html'   
+
+    def get(self, request, course_id):   
+        course = get_object_or_404(Course, id=course_id) 
+        return self.render_to_response({'course':course})  
 
     
