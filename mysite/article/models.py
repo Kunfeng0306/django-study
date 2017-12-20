3b0dc24e70e465a1558a57b9eb12eed56ae6604b
lilyef2000 <lilyef2000@gmail.com>
2017-12-20 12:31:19 +0800

    delete article

--- mysite/article/models.py
+++ mysite/article/models.py
@@ -30,7 +30,7 @@ class ArticlePost(models.Model):
     slug = models.SlugField(max_length=500)
     column = models.ForeignKey(ArticleColumn, related_name="article_column")
     body = models.TextField()
-    created = models.DateTimeField(default=timezone.now())   
+    created = models.DateTimeField(default=timezone.now)   
     updated = models.DateTimeField(auto_now=True)
     users_like = models.ManyToManyField(User, related_name="articles_like", blank=True)
     article_tag = models.ManyToManyField(ArticleTag, related_name='article_tag', blank=True)
